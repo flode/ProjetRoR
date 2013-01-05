ceaf9332e1181c285f845c5221d0687764d4278a
Florian Scheibner <flo.de@hotmail.de>
2013-01-05 15:31:50 +0100

    create user

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,23 @@ class AuthorsController < ApplicationController
   def index
     @authors = Author.paginate(page: params[:page])
   end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(params[:author])
+    @author.user_id = current_user.id
+    if @author.save
+      flash[:success] = "New author created!"
+      redirect_to @author
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  
+  end
 end
--- spec/requests/authors_pages_spec.rb
+++ spec/requests/authors_pages_spec.rb
@@ -3,9 +3,48 @@ require 'spec_helper'
 describe "Authors" do
 
   subject { page }
-  let(:author) { FactoryGirl.create (:author) }
+  let(:user) { FactoryGirl.create(:user) }
   describe "Profile page with name" do
+    let(:author) { FactoryGirl.create (:author) }
     before { visit authors_path }
     it { should have_content('name') }
+  end
+
+  describe "new author"
+    before(:each) do
+      sign_in user
+      visit '/authors/new'
+    end
+    
+    describe "with invalid information" do
+      it "should not create an author" do
+        expect { click_button submit }.not_to change(Author, :count)
+      end
+      
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Forename",         with: "F"
+        fill_in "Lastname",        with: "L"
+      end
+
+      it "should create an author" do
+        expect { click_button submit }.to change(Author, :count).by(1)
+      end
+      
+      describe "after saving the author" do
+        before { click_button submit }
+        let(:author) { Author.find_by_forename_lastname('F', 'L') }
+
+        it { should have_selector('title', text: author.lastname) }
+#	expect(author.user_id).to eq(user.id)
+        it { should have_selector('div.alert.alert-success', text: 'Author created') }
+    end
   end
 end
