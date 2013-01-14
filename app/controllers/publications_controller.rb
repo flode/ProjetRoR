5dc81cab623d01d942e0d2f46870615574d5bf51
Florian Scheibner <flo.de@hotmail.de>
2013-01-14 14:05:44 +0100

    éditer publication pour seulement les auteurs associé
    pass all tests

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,4 +1,5 @@
 class PublicationsController < ApplicationController
+  before_filter :correct_user, only: [:edit]
 
   def index
     @publications = Publication.paginate(page: params[:page])
@@ -6,6 +7,7 @@ class PublicationsController < ApplicationController
 
   def show
     @publication = Publication.find(params[:id])
+    @correct_user_publication = correct_user_publication
   end
 
   def new
@@ -21,4 +23,31 @@ class PublicationsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @publication = Publication.find(params[:id])
+  end
+
+  def update
+    @publication = Publication.find(params[:id])
+    if @publication.update_attributes(params[:publication])
+      flash[:success] = "Publication updated"
+      redirect_to @publication
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def correct_user
+      redirect_to(publication_path(@publication)) unless correct_user_publication
+    end
+
+    def correct_user_publication
+      @publication = Publication.find(params[:id])
+      @publication.authors.each do |f|
+        return true if current_user != nil && current_user.id == f.user.id
+      end
+      return false
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -70,7 +70,7 @@ describe "index" do
     let(:user) { FactoryGirl.create(:user) }
     before do
       sign_in user
-      visit edit_user_path(user)
+      visit user_path(user)
     end
     it { should have_selector('a',    text: "Add Author") }
   end
